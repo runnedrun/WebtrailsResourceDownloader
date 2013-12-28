1843cc5e852914db687c432fa350e66be3ee0871
David Gaynor <david.gaynor@students.olin.edu>
2013-12-27 21:33:35 -0800

    downloader is just about verified and working with the extension, now just have to add in the simple endpoint for viewpage saving and do a few tweaks

--- routes/resourceDownloader.js
+++ routes/resourceDownloader.js
@@ -3,6 +3,8 @@ AWS = require('aws-sdk');
 AWS.config.update({accessKeyId: process.env.AWS_KEY, secretAccessKey: process.env.AWS_SECRET});
 awsBucket = new AWS.S3({params: {Bucket: 'TrailsSitesProto'}});
 
+var pg = require('pg');
+
 var request = require('request');
 
 exports.resourceDownloader = function(req, res){
@@ -11,43 +13,56 @@ exports.resourceDownloader = function(req, res){
     console.log("in the resource downloader");
 //    console.log(req);
 
-    new ResourceHandler(req)
+    var site = new Site(req.body.siteID, function(site) {
+        new ResourceHandler(req, site);
+    });
 
     res.send({ message: 'success!' });
 };
 
-ResourceHandler = function(req) {
-    var site = req.body.siteID
-    var resources = req.body.originalToAwsUrlMap
-    var stylesheets= req.body.styleSheets
-    var html = req.body.html
-    var isIframe = req.body.isIframe
-    var revisionNumber = req.body.revision
-    var is_base_revision = req.body.isBaseRevision
-    var character_encoding = req.body.characterEncoding
+ResourceHandler = function(req, site) {
+    var siteId = req.body.siteID;
+    var resources = req.body.originalToAwsUrlMap || [];
+    var stylesheets= req.body.styleSheets || [];
+    var html = req.body.html;
+    var isIframe = req.body.isIframe;
+    var revisionNumber = req.body.revision;
+    var isBaseRevision = req.body.isBaseRevision;
+
+    var callbackTracker = new CallbackTracker(Object.keys(resources).length, Object.keys(stylesheets).length, function() {
+        console.log("is iframe?", isIframe, typeof isIframe);
+        if (isIframe == "false") {
+            console.log("now setting the archive location of the site");
+            site.archiveLocation = absoluteAwsUrl(html[0]);
+            site.revisionNumbers.push(revisionNumber);
+            if (isBaseRevision == "true") site.baseRevisionNumber = parseInt(revisionNumber);
+            site.updateSiteInDb();
+        }
+    });
+
     console.log('in the resource handler');
 
     function mirrorResource(resourceUrl, mirrorPath) {
-        console.log("mirroring " + resourceUrl + " to " + absoluteAwsUrl(mirrorPath));
-//        request.get(resourceUrl).pipe(request.put(awsPutOptions));
         var requestOptions = {
             uri: resourceUrl,
             encoding: null
         }
         request(requestOptions, function(error, response, body) {
             if (!error && response.statusCode == 200) {
-                putDataOns3(mirrorPath, body, response.headers["content-type"]);
+                putDataOns3(mirrorPath, body, response.headers["content-type"], function() {
+                    site.savedResources.push(resourceUrl);
+                    callbackTracker.markResourceAsSaved(resourceUrl);
+                });
             } else {
                 console.log("error downloading from: " + resourceUrl);
+                callbackTracker.markResourceAsSaved();
             }
         })
     }
 
-    function putDataOns3(path, data, contentType) {
-        console.log('saving style sheet with path: ' + path);
+    function putDataOns3(path, data, contentType, callback) {
         var params = {Key: path, Body: data, ACL: "public-read", CacheControl: "max-age=157680000, public" };
         if (contentType) {
-            console.log("Setting content type to: ", contentType);
             params["ContentType"] = contentType;
         }
         awsBucket.putObject(params, function(err, data) {
@@ -56,6 +71,7 @@ ResourceHandler = function(req) {
             } else {
                 console.log("Successfully uploaded to: " + absoluteAwsUrl(path));
             }
+            callback(data);
         })
     }
 
@@ -64,15 +80,158 @@ ResourceHandler = function(req) {
         return "https://s3.amazonaws.com/TrailsSitesProto/" + encodedPath
     }
 
-    for ( path in stylesheets ) {
-        putDataOns3(path, stylesheets[path]);
+    for ( var path in stylesheets ) {
+        if (site.savedStylesheets.indexOf(path) == -1) {
+            putDataOns3(path, stylesheets[path], "text/css", function(path) {
+                return function() {
+                    site.savedStylesheets.push(path);
+                    callbackTracker.markStylesheetAsSaved();
+                }
+            }(path));
+        } else {
+            console.log("stylesheet already saved, skipping");
+            callbackTracker.markStylesheetAsSaved();
+        }
     }
 
-    for ( resourceUrl in resources ) {
-        mirrorResource(resourceUrl, resources[resourceUrl]);
+    for ( var resourceUrl in resources ) {
+        if (site.savedResources.indexOf(resourceUrl) == -1) {
+            mirrorResource(resourceUrl, resources[resourceUrl]);
+        } else {
+            console.log("resource already saved, skipping");
+            callbackTracker.markResourceAsSaved();
+        }
     }
 
-    putDataOns3(html[0], html[1]);
+    var htmlPathWithRevision = html[0][-1] == "/" ? html[0] + revisionNumber : html[0] + "/" + revisionNumber;
 
+    putDataOns3(htmlPathWithRevision, html[1], "text/html", function() {
+        callbackTracker.markHtmlAsSaved();
+    });
+}
 
+CallbackTracker = function(resourcesRemaining, stylesheetsRemaining, callback) {
+    var htmlSaved = false;
+
+    this.markResourceAsSaved = function() {
+        resourcesRemaining -= 1;
+        checkIfMirroringIsComplete();
+    };
+
+    this.markStylesheetAsSaved = function() {
+        stylesheetsRemaining -= 1;
+        checkIfMirroringIsComplete();
+    };
+
+    this.markHtmlAsSaved = function() {
+        htmlSaved = true;
+        checkIfMirroringIsComplete();
+    };
+
+    function checkIfMirroringIsComplete() {
+        console.log("stylesheets remaining is:" + stylesheetsRemaining);
+        console.log("resources remaining is:" + resourcesRemaining);
+        console.log("htmlSaved is:" + htmlSaved);
+        if (stylesheetsRemaining <= 0 &&
+            resourcesRemaining <= 0 &&
+            htmlSaved == true) {
+            console.log("mirroring complete");
+            callback();
+        }
+    }
+}
+
+var Site  = function(id, onLoaded) {
+    var selectSiteQuery = "\
+        SELECT \
+            revision_numbers, \
+            saved_stylesheets, \
+            saved_resources,\
+            base_revision_number,\
+            archive_location\
+        FROM \
+            sites \
+        WHERE \
+            id = $1";
+
+    var updateSiteStatement = "\
+    UPDATE sites \
+    SET \
+        revision_numbers = $1, \
+        saved_stylesheets = $2, \
+        saved_resources = $3,\
+        base_revision_number = $4,\
+        archive_location = $5\
+    WHERE \
+        id = $6\
+    "
+
+    var site;
+    this.savedResources = [];
+    this.savedStylesheets = [];
+    this.revisionNumbers = [];
+    this.archiveLocation = false;
+    this.baseRevisionNumber = false;
+
+    var thisSite = this;
+
+    this.updateSiteInDb = function() {
+        var queryParams = [thisSite.revisionNumbers.join(","),
+            thisSite.savedStylesheets.join(","),
+            thisSite.savedResources.join(","),
+            thisSite.baseRevisionNumber,
+            thisSite.archiveLocation,
+            id];
+
+        if (!((typeof thisSite.baseRevisionNumber) === "number") || !thisSite.archiveLocation) {
+            console.log("please set archive location and baseRevisionNumber and ensure correct types (string and int) before saving");
+            console.log("archive location is: ", thisSite.archiveLocation);
+            console.log("base revision number is: ", thisSite.baseRevisionNumber)
+            console.log("type of base revision number is: ", (typeof thisSite.baseRevisionNumber))
+            console.log("bool is: ", !(typeof thisSite.baseRevisionNumber) === "number");
+            console.log("bool2 is: ", !thisSite.archiveLocation);
+        } else {
+            makePgQuery(updateSiteStatement, queryParams, function(res) {
+                console.log("site with id:" + id + " updated successfully");
+            });
+        }
+    };
+
+    function loadSiteFromDb() {
+        makePgQuery(selectSiteQuery, [id], function(res) {
+            site = res.rows[0];
+            thisSite.savedResources = trimArray(site.saved_resources.split(","));
+            thisSite.savedStylesheets = trimArray(site.saved_stylesheets.split(","));
+            thisSite.revisionNumbers = trimArray((site.revision_numbers || "").split(","));
+            thisSite.baseRevisionNumber = site.base_revision_number;
+            thisSite.archiveLocation = site.archive_location;
+
+            onLoaded(thisSite);
+        });
+    }
+
+    function trimArray(array) {
+        return array[0] == "" ? array.splice(1) : array;
+    }
+
+    console.log("about to connect to db");
+    function makePgQuery(query, parameters, callback) {
+        pg.connect(process.env.DATABASE_URL, function(err, client, done){
+            if(err) {
+                return console.error('error fetching client from pool', err);
+            } else {
+                client.query(query, parameters, function(err, res) {
+                    done();
+                    if (err) {
+                        console.error("error when query for site");
+                        return console.error(err);
+                    } else {
+                        callback(res)
+                    }
+                });
+            }
+        });
+    }
+
+    loadSiteFromDb();
 }
